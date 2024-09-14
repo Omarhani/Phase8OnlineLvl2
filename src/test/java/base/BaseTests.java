a24883dba7e180cd3f148ebb93113d91ca45ae2f
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-09-14 11:42:26 +0300

    Session 3.2

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,9 +3,13 @@ package base;
 import data.DataModal;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import pages.HomePage;
 import reader.ReadDataFromJson;
 
@@ -19,11 +23,33 @@ public class BaseTests {
 
     protected HomePage homePage;
 
+    ChromeOptions chromeOptions;
+
+    FirefoxOptions firefoxOptions;
+
+    @Parameters("browser")
     @BeforeClass
-    public void setUp(){
-        driver = new ChromeDriver();
+    public void setUp(String browser){
+        setUpBrowser(browser);
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
+    }
+
+    @Parameters("browser")
+    public void setUpBrowser(String browser){
+        if (browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("headlessChrome")) {
+            chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);
+        } else if (browser.equalsIgnoreCase("headlessFirefox")) {
+            firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--headless");
+            driver = new FirefoxDriver(firefoxOptions);
+        }
     }
 
     @AfterClass
