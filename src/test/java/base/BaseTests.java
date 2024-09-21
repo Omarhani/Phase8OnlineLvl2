dccff85a01af3fb44c5fd522f129e3f5e85b47d5
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-09-21 12:03:19 +0300

    Session 4.2 change cdn domain from .net to .xyz

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
 import reader.ReadDataFromJson;
@@ -32,15 +33,15 @@ public class BaseTests {
 
     @Parameters("browser")
     @BeforeClass
-    public void setUp(@Optional ("chrome")String browser){
+    public void setUp(@Optional("chrome") String browser) {
         setUpBrowser(browser);
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
     }
 
     @Parameters("browser")
-    public void setUpBrowser(@Optional ("chrome") String browser){
-        if (browser.equalsIgnoreCase("chrome")){
+    public void setUpBrowser(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
@@ -56,21 +57,34 @@ public class BaseTests {
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
     @AfterMethod
-    public void afterMethod(Method method) throws Exception {
+    public void afterMethod(Method method, ITestResult result) throws Exception {
         utilsTests = new UtilsTests(driver);
         utilsTests.takeScreenShot(method);
         ScreenRecorderUtil.stopRecord();
+        utilsTests.setStatus(method, result);
     }
+
     @BeforeMethod
     public void goHome(Method method) throws Exception {
         driver.get(dataModal().URL);
         ScreenRecorderUtil.startRecord(method.getName());
     }
 
+    @BeforeSuite
+    public void beforeSuite() {
+        utilsTests = new UtilsTests(driver);
+        utilsTests.createReport();
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        utilsTests = new UtilsTests(driver);
+        utilsTests.flushReport();
+    }
 
 }
